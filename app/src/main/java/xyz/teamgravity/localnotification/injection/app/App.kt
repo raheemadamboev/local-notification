a8f77baeca867aa0587d8f29b7942dd07e1fc8de
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-15 21:29:10 +0500

    create notification channel

--- app/src/main/java/xyz/teamgravity/localnotification/injection/app/App.kt
+++ app/src/main/java/xyz/teamgravity/localnotification/injection/app/App.kt
@@ -2,6 +2,18 @@ package xyz.teamgravity.localnotification.injection.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import xyz.teamgravity.localnotification.core.notification.CounterNotification
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application()
+class App : Application() {
+
+    @Inject
+    lateinit var notification: CounterNotification
+
+    override fun onCreate() {
+        super.onCreate()
+
+        notification.createChannel()
+    }
+}
